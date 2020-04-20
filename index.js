1f2e310e9ac5ec6f2a69e5026173ded937eb4c37
Yılmaz Durmaz <yilmaz_durmaz@hotmail.com>
2020-04-20 15:45:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ document.body.innerHTML="Hello, please open Developer Tools , possibly F12"
 
 class Tree{
   root=null;
-  constructor(val){
-    addNode(new Node(val));
+  constructor(value){
+    if(value) {
+      console.log("new tree with initial value");
+      addValue(value)
+    }else{
+      console.log("empty tree started")
+    }
   }
-  addNode(node){
+  addValue(value){
+    let node=new Node(value);
     if(!this.root) {
-      console.log("adding node");
+      console.log("adding value");
       this.root=node;
       return;
-    }
-    if(this.root.val<node.val){
-      console.log("adding left");
-      this.root.left=node;
     }else{
-      console.log("adding right");
-      this.right=new Node(node);
+      console.log("adding node");
+      this.root.addNode(node)
     }
   }
 }
@@ -28,9 +30,32 @@ class Node{
   constructor(value){
     this.value=value;
   }
-
+  addNode(node){
+    //check left and add
+    if(node.value< this.value){
+      console.log(""adding left");
+      if(!this.left){
+        this.left=node;
+      }else{this.left.addNode(node)}
+    }else{
+      console.log("adding right");
+      if(!this.right){
+        this.right=node;
+      }else{this.right.addNode(node)}
+    }
+  }
 }
 
 
 let n= new Node(5);
 console.log(n);
+
+let t = new Tree(3);
+console.log(t)
+
+let tree=new Tree();
+tree.AddValue(5);
+tree.AddValue(3);
+tree.AddValue(6);
+tree.AddValue(5);
+tree.AddValue(7);
